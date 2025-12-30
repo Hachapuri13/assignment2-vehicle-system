d725b80f9a0f0391fbd22916766877d0b99c7317
Hachapuri13 <k.potato4ver@gmail.com>
2025-12-30 07:30:20 +0500

    feat(vehicle): added Vehicle constructor and subclass structure

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,10 @@
 public abstract class Vehicle {
+    protected String brand;
+    protected int year;
+    protected Driver driver;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
 }
