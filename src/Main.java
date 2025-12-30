e5fabeaa9e4e6ef0f31cafdf4a3e0a37ec728882
Hachapuri13 <k.potato4ver@gmail.com>
2025-12-30 07:49:26 +0500

    feat(main): added Main program and array processing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Driver driver1 = new Driver("Alex", "KZ12345");
+        Driver driver2 = new Driver("Dana", "KZ67890");
+
+        Vehicle car = new Car("Toyota", 2020, 4, "Petrol");
+        Vehicle motorcycle = new Motorcycle("Yamaha", 2018, false);
+        Vehicle truck = new Truck("Volvo", 2015, 12.5, 4);
+
+        car.setDriver(driver1);
+        motorcycle.setDriver(driver1);
+        truck.setDriver(driver2);
+
+        Vehicle[] vehicles = {car, motorcycle, truck};
+
+        for (Vehicle v : vehicles) {
+            v.startEngine();
+            v.displayInfo();
+
+            if (v.getDriver() != null) {
+                v.getDriver().displayDriverInfo();
+            }
+
+            v.stopEngine();
+            System.out.println("----------------------");
+        }
+    }
 }
