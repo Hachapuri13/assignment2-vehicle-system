4e23639564cf7dfb6e10959799662bd9b98e85fd
Hachapuri13 <k.potato4ver@gmail.com>
2025-12-30 07:44:45 +0500

    feat(vehicle): implemented superclass and subclasses

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,5 @@
 public abstract class Vehicle {
+
     protected String brand;
     protected int year;
     protected Driver driver;
@@ -6,5 +7,20 @@ public abstract class Vehicle {
     public Vehicle(String brand, int year) {
         this.brand = brand;
         this.year = year;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public abstract void startEngine();
+    public abstract void stopEngine();
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
     }
 }
